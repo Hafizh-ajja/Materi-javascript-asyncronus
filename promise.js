405b56b7c745fd09d0f5680a779786cd8d59a1a2
Hafizh-ajja <hhafizh662@email.com>
2026-04-16 11:31:16 +0700

    pembelajaran

--- promise.js
+++ promise.js
@@ -5,19 +5,34 @@ function ambilData() {
         setTimeout(() => {
 
             const sukses = false; //(true & false) untuk melihat sukses dan gagal
-            if(sukses) {
+            if (sukses) {
                 resolve("Data berhasil diambil");  //kirim ke .then
             } else {
                 reject("Gagal ambil data");
             }
-        },3000)
+        }, 3000)
     });
 };
 
 ambilData()
-.then((hasil) => {
-    console.log(`hasil: ${hasil}`);
+    .then((hasil) => {
+        console.log(`hasil: ${hasil}`);
+    })
+    .catch((error) => {
+        console.error(`Error: ${error}`);
+    });
+
+
+// Real Case Promise
+function getUser() {
+    return fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then(Response => Response.json()); // convert ke json
+};
+
+    getUser()
+.then(data => {
+    console.log(`${data.name}`);
 })
-.catch((error) => {
-    console.error(`Error: ${error}`);
+.catch(err => {
+    console.error(`${err}`);
 });
